66fb2d119f6dae35db09e55ab4b9e0decc6a194b
Chris Browder <rcbrowder@gmail.com>
2018-05-25 08:53:06 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('copycss', function() {
+	return gulp.src('app/css/**/*.css')
+		.pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css/'))
+});
